c87aa4ba16f20cd02fe177676ff082540fe7fbb4
Juno Kim <lippoint.surf0622@gmail.com>
2023-08-17 17:38:40 +0900

    Next js installed

---
+++ project02/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div>
+        <h1 className="text-2xl">Project02</h1>
+      </div>
+    </main>
+  );
+}
