2d548789b363bfd2cb00917c6037e5661a5b57e9
Juno Kim <lippoint.surf0622@gmail.com>
2023-08-17 17:46:08 +0900

    Modify next config.js

--- project01/next.config.js
+++ project01/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/project02/',
+        destination: process.env.PROJECT2_URL,
+      },
+      {
+        source: '/project02/:match*',
+        destination: process.env.PROJECT2_URL + ':match',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- project02/next.config.js
+++ project02/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: env.ASSET_PREFIX,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
